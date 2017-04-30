9a396b7bddb95afc0edcd467107b2747cb015428
James Schrader <jschrader@madisonchildrensmuseum.org>
2017-04-29 20:11:37 -0500

    Import components 'Card' and 'CardSection' into AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
-    <View>
-        <Text>{props.album.title}</Text>
-    </View>
-);
+const AlbumDetail = (props) => {
+    return (
+        <Card>
+            <CardSection>
+                <Text>{props.album.title}</Text>
+            </CardSection>
+        </Card>
+    );
+};
 
 export default AlbumDetail;
